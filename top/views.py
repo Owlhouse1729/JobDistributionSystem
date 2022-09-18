d5f8fbde311123a00fd4be680cdc3222f2402fc7
SerpentineK <m.kazuto1002@gmail.com>
2022-09-18 20:11:52 +0900

    2022-09-18

--- top/views.py
+++ top/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import *
 from django.contrib.auth.views import LoginView, LogoutView
-from .form import LoginForm
+from .form import LoginForm, RegisterForm
+from django.urls import reverse_lazy
+from .models import User
+from .mixins import OnlyEmployerMixin
 
 
 # Create your views here.
