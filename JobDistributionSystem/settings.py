cbd8e1a825f50af7a36776135e480f7befd991d4
Owlhouse <takujomu@gmail.com>
2022-09-28 15:12:46 +0900

    heroku settings

--- JobDistributionSystem/settings.py
+++ JobDistributionSystem/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,4 +150,10 @@ if not DEBUG:
     MIDDLEWARE += [
         'whitenoise.middleware.WhiteNoiseMiddleware',
     ]
+    db_from_env = dj_database_url.config()
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
+    ALLOWED_HOSTS = ['*']
+
     django_heroku.settings(locals())
